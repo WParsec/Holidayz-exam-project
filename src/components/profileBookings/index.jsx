2be35ec1e1c58e9b780e44f0e6a24f37e3a0d929
WParsec <91538926+WParsec@users.noreply.github.com>
2023-05-26 15:36:53 +0200

    links from bookings to venue on profile

--- src/components/profileBookings/index.jsx
+++ src/components/profileBookings/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // Import styles and assets
 import styles from './profileBookings.module.scss';
@@ -60,17 +61,16 @@ function ProfileBookings({ url, accessToken }) {
   return (
     <div className="container">
       <div className={styles.bookings_wrap}>
-        <div
-          tabIndex="0"
-          className={styles.left}
-          onClick={() => setShowPrevious(!showPrevious)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              setShowPrevious(!showPrevious);
-            }
-          }}
-        >
-          <h4 className={styles.h4}>
+        <div tabIndex="0" className={styles.left}>
+          <h4
+            className={styles.h4}
+            onClick={() => setShowPrevious(!showPrevious)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setShowPrevious(!showPrevious);
+              }
+            }}
+          >
             Your previous bookings <span>View</span>
           </h4>
           <div
@@ -78,35 +78,36 @@ function ProfileBookings({ url, accessToken }) {
           >
             {bookings && bookings.length > 0
               ? previousBookings.map((booking) => (
-                  <div className={styles.booking} key={booking.id}>
-                    <div className={styles.image_div}>
-                      <img
-                        src={booking.venue.media[0]}
-                        alt={booking.venue.name}
-                      />
+                  <Link to={`/venue/${booking.venue.id}`} key={booking.id}>
+                    <div className={styles.booking}>
+                      <div className={styles.image_div}>
+                        <img
+                          src={booking.venue.media[0]}
+                          alt={booking.venue.name}
+                        />
+                      </div>
+                      <div className={styles.info}>
+                        <h5 className={styles.h5}>{booking.venue.name}</h5>
+                        {booking.venue.location.address && (
+                          <p>{booking.venue.location.address}</p>
+                        )}
+                      </div>
                     </div>
-                    <div className={styles.info}>
-                      <h5 className={styles.h5}>{booking.venue.name}</h5>
-                      {booking.venue.location.address && (
-                        <p>{booking.venue.location.address}</p>
-                      )}
-                    </div>
-                  </div>
+                  </Link>
                 ))
               : ''}
           </div>
         </div>
-        <div
-          tabIndex="0"
-          className={styles.right}
-          onClick={() => setShowUpcoming(!showUpcoming)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              setShowUpcoming(!showUpcoming);
-            }
-          }}
-        >
-          <h4 className={styles.h4}>
+        <div tabIndex="0" className={styles.right}>
+          <h4
+            className={styles.h4}
+            onClick={() => setShowUpcoming(!showUpcoming)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setShowUpcoming(!showUpcoming);
+              }
+            }}
+          >
             Your upcoming bookings <span>View</span>
           </h4>
           <div
@@ -114,26 +115,31 @@ function ProfileBookings({ url, accessToken }) {
           >
             {bookings && bookings.length > 0
               ? upcomingBookings.map((booking) => (
-                  <div className={styles.booking} key={booking.id}>
-                    <div className={styles.image_div}>
-                      <img
-                        src={booking.venue.media[0]}
-                        alt={booking.venue.name}
-                      />
+                  <Link to={`/venue/${booking.venue.id}`} key={booking.id}>
+                    <div className={styles.booking}>
+                      <div className={styles.image_div}>
+                        <img
+                          src={booking.venue.media[0]}
+                          alt={booking.venue.name}
+                        />
+                      </div>
+                      <div className={styles.info}>
+                        <h5 className={styles.h5}>{booking.venue.name}</h5>
+                        {booking.venue.location.address && (
+                          <p>{booking.venue.location.address}</p>
+                        )}
+                      </div>
+                      <div className={styles.dates}>
+                        <p>
+                          From:{' '}
+                          {new Date(booking.dateFrom).toLocaleDateString()}
+                        </p>
+                        <p>
+                          To: {new Date(booking.dateTo).toLocaleDateString()}
+                        </p>
+                      </div>
                     </div>
-                    <div className={styles.info}>
-                      <h5 className={styles.h5}>{booking.venue.name}</h5>
-                      {booking.venue.location.address && (
-                        <p>{booking.venue.location.address}</p>
-                      )}
-                    </div>
-                    <div className={styles.dates}>
-                      <p>
-                        From: {new Date(booking.dateFrom).toLocaleDateString()}
-                      </p>
-                      <p>To: {new Date(booking.dateTo).toLocaleDateString()}</p>
-                    </div>
-                  </div>
+                  </Link>
                 ))
               : ''}
           </div>
